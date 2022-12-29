314b837154812e12df10d6f15747ed87c59a6be4
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2022-12-30 00:46:33 +0100

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+export const About = () => {
+  return <div>Om meg</div>;
+};
+
+export default About;
---
+++ pages/projects/[slug].tsx
@@ -0,0 +1,5 @@
+export const Project = () => {
+  return <div>Dette er et prosjekt med slug ...</div>;
+};
+
+export default Project;
